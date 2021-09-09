5c33d451d5f3aa45a1ab68c1173334e9eb09a5a2
Andrieli Luci <andrieliluci@gmail.com>
2021-09-09 09:07:45 -0300

    Atualização - Média Escola  (09-09-2021)

--- media-escolar/media-escola1.py
+++ media-escolar/media-escola1.py
@@ -1,16 +1,26 @@
 notas = []
+soma = 0
+
+def mensagem(texto):
+
+    print('✧----' * 12)
+    print(texto.center(64))
+    print('✧----' * 12)
+
+
+mensagem('Média Escolar')
 
 for item in range(1, 5):
 
     while True:
-        n = float(input(f'{item}ª Nota: '))
+        n = float(input(f' >   {item}ª Nota: '))
 
         if n >= 0 and n <= 10:
             break
 
     notas.append(n)
+    soma += n
 
-soma = sum(notas)
 
 if soma == 0:
     media = 0
@@ -25,6 +35,8 @@ else:
     else:
         msg = 'Você foi aprovado(a)! :)'
 
+mensagem('Resultado Final: ')
+
 for i, item in enumerate(notas):
 
     if (i == 0):
@@ -32,5 +44,6 @@ for i, item in enumerate(notas):
 
     print(item, end=' | ')
 print(f'-> {media:.2f} ->', msg)
+
 
 
