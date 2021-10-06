5ea29893b4ff312455700afa1f41119efe0e9811
Andrieli Luci <andrieliluci@gmail.com>
2021-10-06 16:36:12 -0300

    Tratamento de erros

--- festa-da-escola/lib/estoque/__init__.py
+++ festa-da-escola/lib/estoque/__init__.py
@@ -1,14 +1,7 @@
 from lib import style as s
 
-options = ['Comprar fichas', 'Gastar fichas', 'Ver o Estoque']
-
-def apresentarMenu():
+def apresentarMenu(list):
     s.formatarTexto('MENU', type='menu_title')
 
-    for i, item in enumerate(options):
+    for i, item in enumerate(list):
         print(f'[{i + 1}] : {item}')
-
-    print('-' * 50)
-
-    op = input('> Escolha a opção desejada: ')
-    return op
--- festa-da-escola/main.py
+++ festa-da-escola/main.py
@@ -1,11 +1,23 @@
 from lib import style as s
 from lib import estoque as e
 
+options = ['Comprar fichas', 'Gastar fichas', 'Ver o Estoque']
+
 
 s.formatarTexto('Bem-Vinda(o) à Festa Escolar!', type='title')
-op = e.apresentarMenu()
+e.apresentarMenu(options)
 
-try:
-    int(op)
-except ValueError:
-   print('erro')
+print('-' * 50)
+
+while True:
+
+    try:
+        op = int(input('> Escolha a opção desejada: '))
+    except ValueError:
+        print('[ERRO] - A opção digitada é inválida.')
+    else:
+        if (op > 0 and op <= 3):
+            print(f'Você escolheu: {options[op - 1]}')
+            break
+        else:
+            print('[ERRO] - A opção digitada é inválida.')
