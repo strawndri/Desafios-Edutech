29afad48d0a96cbc56170fe1c4d16975533666e8
Andrieli Luci <andrieliluci@gmail.com>
2021-10-30 15:40:19 -0300

    Configurando pagamento

--- estacionamento/main.py
+++ estacionamento/main.py
@@ -8,18 +8,31 @@ def print_txt(text, type='default'):
 
 
 # função que imprime menu
-def menu():
+def menu(type):
 
     print('-' * 50)
-    print_txt('Veículos', 'menu')
-    print(f' {"Total":<8} {"Ocupadas":<10} {"Preço":9} {"Categoria"}')
 
-    for item in veiculos:
-        for key, value in item.items():
-            print(f'   {value[0]:<8} {value[1]:<8} R${value[2]:<7.2f} {key}')
+    if (type == 'principal'):
+        print_txt('Veículos', 'menu')
+        print(f' {"Total":<8} {"Ocupadas":<10} {"Preço":9} {"Categoria"}')
 
-    print('-' * 50)
-    print(f'Caixa: R$ {caixa:.2f}')
+        for item in veiculos:
+            for key, value in item.items():
+                print(f'   {value[0]:<8} {value[1]:<8} R${value[2]:<7.2f} {key}')
+
+        print('-' * 50)
+        print(f'Caixa: R$ {caixa:.2f}')
+
+    elif (type == 'pagamento'):
+
+        for i, item in enumerate(op_pagamento):
+            print(f'{i + 1} - {item}')
+
+    elif (type == 'finalizacao'):
+
+        for i, item in enumerate(itens_finalizacao):
+            print(f'{i + 1} - {item}')
+
     print('-' * 50)
 
 def realizar_escolha():
@@ -38,27 +51,36 @@ def realizar_escolha():
     for item in veiculos[option - 1].values():
         total = item[0]
         indisponivel = item[1]
+        valor = item[2]
 
     if indisponivel >= total:
         print('Infelizmente estamos com todas as vagas ocupadas.')
-        resposta = False
     elif option == 1:
         print(f'Sua vaga é: {indisponivel + 1}')
-        resposta = True
     else:
-
         while i < option:
             for item in veiculos[i - 1].values():
                 indisponivel += item[0]
             i += 1
-            
+
         print(f'Sua vaga é: {indisponivel + 1}')
-        resposta = True
+        print(f'Valor a pagar: R$ {valor}')
+
+        menu('pagamento')
+
+        op = int(input(f'> Opção escolhida: '))
+
+        if (op == 1):
+            resposta = True
+        elif (op == 2):
+            resposta = False
+        elif (op == 3):
+            sistema()
 
     return resposta, option
 
 
-def pagamento(option, caixa):
+def pagamento(caixa, option):
 
     if (option == 1):
         caixa += 5
@@ -69,9 +91,14 @@ def pagamento(option, caixa):
     else:
         caixa += 50
 
-# Programa ---------
+def sistema():
+    print_txt('Bem-vinda(o) ao nosso Estacionamento!', type='title')
+    menu('principal')
+    resposta, option = realizar_escolha()
+    print(resposta)
+    if (resposta == True):
+        pagamento(caixa, option)
 
-# variáveis importantes do sistema
 
 # A ordem da lista dos veículos é: total de vagas / vagas ocupadas / preço
 veiculos = [
@@ -80,11 +107,18 @@ veiculos = [
     {'Médio Porte': [20, 0, 20.00]},
     {'Grande Porte': [10, 0, 50.00]}]
 
+op_pagamento = ['Finalizar Pagamento', 'Cancelar Pagamento', 'Escolher outra opção']
+itens_finalizacao = ['Continuar Sistema', 'Fechar estacionamento']
+
 caixa = 0
 
-print_txt('Bem-vinda(o) ao nosso Estacionamento!', type='title')
-menu()
-resposta, option = realizar_escolha()
+while True:
+    sistema()
 
-if (resposta == True):
-    pagamento(caixa, option)
+    menu('finalizacao')
+    opcao = int(input('> Opção Escolhida: '))
+
+    if (opcao == 2):
+        break
+
+
