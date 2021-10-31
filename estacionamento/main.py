4262afdd3b672bdc5215ee4732c929d1abbec7c9
Andrieli Luci <andrieliluci@gmail.com>
2021-10-31 16:14:30 -0300

    Finalização do programa

--- estacionamento/main.py
+++ estacionamento/main.py
@@ -15,28 +15,28 @@ def print_txt(text, type='default'):
 # função que imprime menu
 def menu(type):
 
-    print('-' * 50)
+    print('\n', end='')
+    print('—' * 50)
     print_txt(type, 'title_menu')
 
     if (type == 'principal'):
         print_txt('Veículos', 'menu')
         print(f'  {"N°":<4} {"Total":<7} {"Ocupadas":<10} {"Preço":9} {"Categoria"}')
 
+        # tabela listando os veículos, suas vagas e preços
         for i, item in enumerate(veiculos):
             print(f'  {i + 1:<6}', end='')
             for key, value in item.items():
                 print(f'{value[0]:<9} {value[1]:<7} R${value[2]:<7.2f} {key}')
 
         print('-' * 50)
-        print(f'Caixa: R$ {caixa:.2f}')
+        print(f'Caixa: R$ {sum(caixa):.2f}')
 
     elif (type == 'pagamento'):
-
         for i, item in enumerate(op_pagamento):
             print(f'{i + 1} - {item}')
 
-    elif (type == 'finalizacao'):
-
+    elif (type == 'finalização'):
         for i, item in enumerate(itens_finalizacao):
             print(f'{i + 1} - {item}')
 
@@ -44,15 +44,16 @@ def menu(type):
         print(f'{"":<10} {"Total de Veículos":<20} {"Caixa"}')
 
         total_ocupadas = 0
-        for item in veiculos:
+        for i, item in enumerate(veiculos):
+            valor_estacionamento = caixa[i]
             for key, value in item.items():
                 total_ocupadas += value[3]
-                print(f'{"":<10} {value[3]} - {key:<16} R$ {value[2]:.2f}')
+                print(f'{"":<10} {value[3]} - {key:<16} R$ {valor_estacionamento:.2f}')
 
         print(f'{"-" * 30}'.center(50))
-        print(f'{"":<10} {total_ocupadas} - Veículos {" ":<7} R$ {caixa:.2f}')
+        print(f'{"":<10} {total_ocupadas} - Veículos {" ":<7} R$ {sum(caixa):.2f}')
 
-    print('-' * 50)
+    print('—' * 50)
 
 
 # função que exige o número correto a ser digitado
@@ -61,30 +62,34 @@ def tratamento_valor(list):
         try:
             option = int(input('> Digite a opção desejada: '))
         except ValueError:
-           print('Tente novamente: ', end='')
+           print(' ┈ ┈ (!) Tente novamente: valor inválido (!) ┈ ┈ ')
         else:
             if (option >= 1 and option <= len(list)):
                 break
             else:
-                print(f'O valor precisa ser entre 1 e {len(list)}')
+                print(f' ┈ ┈ (!) O valor precisa ser entre 1 e {len(list)} (!) ┈ ┈ ')
 
     return option
 
+# função que realiza o pagamento do estacionamento
 def pagamento(caixa, option):
 
     for item in veiculos[option - 1].values():
-        caixa = item[2]
+        valor_estacionamento = item[2]
 
-    return caixa
+    return valor_estacionamento
 
 
 #função principal
-def sistema():
+def cadastrar_vaga():
+
     menu('principal')
     total, indisponivel, i = 0, 0, 1
 
     option = tratamento_valor(veiculos)
 
+
+    #salva o total de vagas, as indisponíveis e o valor a pagar
     for item in veiculos[option - 1].values():
         total = item[0]
         indisponivel = item[1]
@@ -92,19 +97,22 @@ def sistema():
 
         print(total, indisponivel)
 
+    vaga = indisponivel + 1
+
     #computando a vaga no sistema
     if indisponivel >= total:
         print('Infelizmente estamos com todas as vagas ocupadas.')
         valor = 0
     elif option == 1:
-        print(f'Sua vaga é: {indisponivel + 1}')
+        print(f'Sua vaga é: {vaga}')
     else:
         while i < option:
             for item in veiculos[i - 1].values():
                 indisponivel += item[0]
             i += 1
 
-        print(f'Sua vaga é: {indisponivel + 1}')
+        vaga = indisponivel + 1
+        print(f'Sua vaga é: {vaga}')
 
     print(f'Valor a pagar: R$ {valor:.2f}')
     menu('pagamento')
@@ -112,16 +120,19 @@ def sistema():
     option2 = tratamento_valor(op_pagamento)
 
     if (option2 == 1):
+        # organiza as vagas ocupadas e o total do dia
         for item in veiculos[option - 1].values():
             item[1] = item[1] + 1
             item[3] = item[3] + 1
-        resposta = True
-    elif (option2 == 2):
-        resposta = False
-    elif (option2 == 3):
-        sistema()
+        resposta = True # cliente confirmou o pagamento
 
-    return resposta, option
+    elif (option2 == 2):
+        resposta = False # cliente cancelou o pagamento
+
+    elif (option2 == 3):
+        cadastrar_vaga() # cliente optou por outra classificação
+
+    return resposta, option, vaga
 
 
 # A ordem da lista dos veículos é: total de vagas / vagas ocupadas / preço / entrada de veiculos
@@ -133,38 +144,45 @@ veiculos = [
 
 op_pagamento = ['Finalizar Pagamento', 'Cancelar Pagamento', 'Escolher outra opção']
 itens_finalizacao = ['Continuar Sistema', 'Remover vaga', 'Fechar estacionamento']
-caixa = 0
+caixa = [0, 0, 0, 0]
 total_ocupadas = 0
 
 while True:
 
     print_txt('Bem-vinda(o) ao nosso Estacionamento!', type='title')
-    resposta, option = sistema()
+    resposta, option, vaga = cadastrar_vaga()
 
+    # adiciona o pagamento ao caixa
     if (resposta == True):
-        caixa += pagamento(caixa, option)
+        caixa[option - 1] += pagamento(caixa, option)
+
+    # processamento cancelado
     elif (resposta == False):
         break
 
     sleep(1)
 
-    menu('finalizacao')
+    # finalicação do programa
+    menu('finalização')
     option3 = tratamento_valor(itens_finalizacao)
 
     sleep(1)
 
-    if (option == 2):
+    # remove a vaga atual
+    if (option3 == 2):
         for item in veiculos[option - 1].values():
-            print('entrou op 2')
             item[1] = item[1] - 1
+            print(f'Liberando a vaga {vaga}...')
 
+    # finaliza o programa e apresenta o resultado final do dia
     elif (option3 == 3):
-
         for item in veiculos:
             for i in item.values():
                 total_ocupadas += i[1]
 
         menu('resultado final')
         break
+
+print_txt('Obrigada por utilizar o nosso serviço!', type='title')
 
 
